a3c2f264ecd3e5f00c46fc6deddb3d36861c9f53
Zhihao Chen <zh-ch14@mails.tsinghua.edu.cn>
2017-03-18 20:38:39 +0800

    Update start.sh

--- Shadowsocks-libev/start.sh
+++ Shadowsocks-libev/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+
+### BEGIN INIT INFO
+# Provides:          Yiph_Goth
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: shadowsocks
+# Description:       shadowsocks-libev autostart
+### END INIT INFO
+
+
 cd /etc/shadowsocks-libev
 ss-server -c config.json -u -d 8.8.8.8 --fast-open -a nobody 2>&1 &
